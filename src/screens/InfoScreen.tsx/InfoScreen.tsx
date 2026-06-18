50262d7d1e79d8092b3fbab1cdae2f4e647069bf
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-18 19:09:13 -0300

    adicionando tela de informação

---
+++ src/screens/InfoScreen.tsx/InfoScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from 'react-native'
+import styles from './styles'
+
+export default function InfoScreen(){
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Sobre os Desenvolvedores</Text>
+        </View>
+    )
+}
+
---
+++ src/screens/InfoScreen.tsx/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1A1A2E',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
+
+export default styles;
