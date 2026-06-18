3fe757de213cc32a1e7b6540bcd26c146e52a2d3
elionardosantos <elionars@gmail.com>
2026-06-18 08:33:20 -0300

    atualizando a branch

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { TabNavigator } from './src/utils/routes/tab.routes';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TabNavigator } from "./src/utils/routes/tab.routes";
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <TabNavigator />
-    </>
+    </SafeAreaProvider>
   );
 }
