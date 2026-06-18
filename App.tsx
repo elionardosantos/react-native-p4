63f33ec386fb88c39e70bf54af4825bdfb37e34b
elionardosantos <elionars@gmail.com>
2026-06-18 08:42:14 -0300

    feat: adiciona o WeatherProvider na main

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TabNavigator } from "./src/utils/routes/tab.routes";
+import { WeatherProvider } from "./src/context/WeatherContext";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <TabNavigator />
-    </SafeAreaProvider>
+    <WeatherProvider>
+      <SafeAreaProvider>
+        <TabNavigator />
+      </SafeAreaProvider>
+    </WeatherProvider>
   );
 }
