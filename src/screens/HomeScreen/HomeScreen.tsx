1d0951bf63e269f1713b4f5004574648051ef739
Bernardo da Silva <bsilvaaoliveira@gmail.com>
2026-06-20 20:12:26 -0300

    feat:Aba de QuickCities ja com rota.

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
-import { ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import WeatherHeader from '../../components/WeatherHeader/WeatherHeader';
-import WeatherStats from '../../components/WeatherStats/WeatherStats';
-import { HourlyForecast } from '../../components/HourlyForecast/HourlyForecast';
-import { WeeklyForecast } from '../../components/WeeklyForecast/WeeklyForecast';
-import { useWeather } from '../../context/WeatherContext';
-import styles from './styles';
+import React, { useEffect } from "react";
+import { ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import WeatherHeader from "../../components/WeatherHeader/WeatherHeader";
+import WeatherStats from "../../components/WeatherStats/WeatherStats";
+import { HourlyForecast } from "../../components/HourlyForecast/HourlyForecast";
+import { WeeklyForecast } from "../../components/WeeklyForecast/WeeklyForecast";
+import { useWeather } from "../../context/WeatherContext";
+import styles from "./styles";
+import { QuickCities } from "../../components/QuickCities/QuickCities";
 
 export default function HomeScreen() {
   const { loadWeather } = useWeather();
@@ -21,7 +22,8 @@ export default function HomeScreen() {
         <WeatherHeader />
         <WeatherStats />
         <HourlyForecast />
-        <WeeklyForecast />        
+        <WeeklyForecast />
+        <QuickCities />
       </ScrollView>
     </SafeAreaView>
   );
